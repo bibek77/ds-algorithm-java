81fe48184f80d5c19a57514b2031596b8224e40b
bibek77 <bibekrana01@gmail.com>
2022-05-28 23:07:18 +0530

    num power recursion code

--- src/main/java/com/github/bibek77/dsa/dataStructures/recursion/NumPower.java
+++ src/main/java/com/github/bibek77/dsa/dataStructures/recursion/NumPower.java
@@ -1,4 +1,22 @@
-package com.github.bibek77.dsa.dataStructures.recursion;/**
+package com.github.bibek77.dsa.dataStructures.recursion;
+
+/**
  * @author bibek
- */public class NumPower {
+ */
+public class NumPower {
+    public static void main(String[] args) {
+        NumPower obj = new NumPower();
+        System.out.println(obj.numpower(3, 2));
+    }
+
+    public int numpower(int base, int exp) {
+        if (exp == 0) {
+            return 1;
+        }
+        if (exp < 0) {
+            return -1;
+        }
+        return base * numpower(base, exp - 1);
+    }
 }
+
