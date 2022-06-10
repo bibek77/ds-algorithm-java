9b8aa69eb3da9d8fe0507ca7304f42b5948f96c1
bibek77 <bibekrana01@gmail.com>
2022-06-10 23:55:27 +0530

    linked list implementations

---
+++ src/main/java/com/github/bibek77/dsa/dataStructures/linkedLists/SingleLLImplement.java
@@ -0,0 +1,15 @@
+package com.github.bibek77.dsa.dataStructures.linkedLists;
+
+/**
+ * @author bibek
+ */
+public class SingleLLImplement {
+
+    public static void main(String[] args) {
+        SingleLinkedList linkedList = new SingleLinkedList();
+        linkedList.createSingleLinkedList(5);
+        System.out.println(linkedList.head.value);
+
+
+    }
+}
