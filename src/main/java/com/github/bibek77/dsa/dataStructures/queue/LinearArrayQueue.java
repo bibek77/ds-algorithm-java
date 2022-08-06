06cc207efcef91292c4b5c7a35cc434826349326
bibek77 <bibekrana01@gmail.com>
2022-08-06 23:23:58 +0530

    linear queue with array

--- src/main/java/com/github/bibek77/dsa/dataStructures/queue/LinearArrayQueue.java
+++ src/main/java/com/github/bibek77/dsa/dataStructures/queue/LinearArrayQueue.java
@@ -9,5 +9,11 @@ public class LinearArrayQueue {
         System.out.println(linearArrayQueueCreate.isFull());
         System.out.println(linearArrayQueueCreate.isEmpty());
         linearArrayQueueCreate.enQueue(56);
+        linearArrayQueueCreate.enQueue(46);
+        linearArrayQueueCreate.enQueue(36);
+        System.out.println(linearArrayQueueCreate.deQueue());
+        System.out.println(linearArrayQueueCreate.deQueue());
+        System.out.println(linearArrayQueueCreate.peek());
+        linearArrayQueueCreate.deleteQueue();
     }
 }
