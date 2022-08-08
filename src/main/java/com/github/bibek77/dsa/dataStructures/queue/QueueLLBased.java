62d6a81e442cf70a3f15d1736998f71a146b69c5
bibek77 <bibekrana01@gmail.com>
2022-08-08 11:19:31 +0530

    Refractored and LL Based Queue Created

---
+++ src/main/java/com/github/bibek77/dsa/dataStructures/queue/QueueLLBased.java
@@ -0,0 +1,15 @@
+package com.github.bibek77.dsa.dataStructures.queue;
+
+import com.github.bibek77.dsa.dataStructures.linkedLists.SingleLinkedList;
+
+/**
+ * @author bibek
+ */
+public class QueueLLBased {
+    SingleLinkedList singleLinkedList;
+
+    public QueueLLBased() {
+        singleLinkedList = new SingleLinkedList();
+        System.out.println("The Queue is successfully created.");
+    }
+}
---
+++ src/main/java/com/github/bibek77/dsa/dataStructures/queue/QueueLLBasedImplement.java
@@ -0,0 +1,10 @@
+package com.github.bibek77.dsa.dataStructures.queue;
+
+/**
+ * @author bibek
+ */
+public class QueueLLBasedImplement {
+    public static void main(String[] args) {
+        QueueLLBased queueLLBased = new QueueLLBased();
+    }
+}
