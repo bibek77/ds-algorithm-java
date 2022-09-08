0082d8bdc4110c59f89d6b4a15e2c517f8fa5364
bibek77 <bibekrana01@gmail.com>
2022-09-08 12:53:23 +0530

    list implement

---
+++ src/main/java/com/github/bibek77/dsa/miscellaneous/LinkedListImplement.java
@@ -0,0 +1,17 @@
+package com.github.bibek77.dsa.miscellaneous;
+
+/**
+ * @author bibek
+ */
+public class LinkedListImplement {
+    public static void main(String[] args) {
+        LinkedLists ll = new LinkedLists();
+        ll.createLL(3);
+        ll.insertNodeLL(4);
+        ll.insertNodeLL(1);
+        ll.insertNodeLL(8);
+        ll.insertNodeLL(5);
+        ll.traverseLL();
+    }
+
+}
