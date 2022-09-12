ad8e485e34c2793db0a14c3484c960644225efbe
bibek77 <bibekrana01@gmail.com>
2022-09-13 01:29:23 +0530

    tap water

--- src/main/java/com/github/bibek77/dsa/practice/TrappingRainWater42.java
+++ src/main/java/com/github/bibek77/dsa/practice/TrappingRainWater42.java
@@ -20,11 +20,11 @@ public class TrappingRainWater42 {
         int maxRight = 0;
         int len = height.length;
         for (int i = 0; i < len - 1; i++) {
-            maxLeft = i!=0 ? Math.max(maxLeft, height[i-1]) : 0;
+            maxLeft = i!=0 ? Math.max(maxLeft, height[i-1]) : 0; //Max Left,
             for (int j = i+1; j < len; j++) {
-                maxRight = Math.max(maxRight, height[j]);
+                maxRight = Math.max(maxRight, height[j]); // Max Right
             }
-            totalWater = totalWater + (Math.max(Math.min(maxLeft, maxRight) - height[i], 0));
+            totalWater = totalWater + (Math.max(Math.min(maxLeft, maxRight) - height[i], 0)); //Add water only if not negative
             maxRight = 0;
         }
         return totalWater;
