2a208db23fb928d971b7912a50fdcbc2655d6205
bibek77 <bibekrana01@gmail.com>
2022-09-16 08:42:10 +0530

    string optimal

--- src/main/java/com/github/bibek77/dsa/practice/BackspaceString844.java
+++ src/main/java/com/github/bibek77/dsa/practice/BackspaceString844.java
@@ -8,8 +8,37 @@ import java.util.Stack;
 public class BackspaceString844 {
     public static void main(String[] args) {
 
-        System.out.println(backspaceCompareBrute("ab#c","ad#c"));
+        System.out.println(backspaceCompareBrute("ab#c", "ad#c"));
+        System.out.println(backspaceCompareOptimal("ab#c", "ad#c"));
 
+    }
+
+    public static boolean backspaceCompareOptimal(String s, String t) {
+
+        int i = s.length()-1;
+        int j = t.length()-1;
+        int skip = 0;
+        char []sc = s.toCharArray();
+        char []tc = t.toCharArray();
+        while (true) {
+            skip=0;
+            while(i>=0 && (skip>0 || sc[i]=='#')) {
+                skip += sc[i]=='#' ? 1:-1;
+                i--;
+            }
+            skip=0;
+            while(j>=0 && (skip>0 || tc[j]=='#')) {
+                skip += tc[j]=='#' ? 1:-1;
+                j--;
+            }
+            if(i>=0 && j>=0 && sc[i]==tc[j]) {
+                i--;
+                j--;
+            } else {
+                break;
+            }
+        }
+        return i==-1 && j==-1;
     }
 
     public static boolean backspaceCompareBrute(String s, String t) {
@@ -20,25 +49,25 @@ public class BackspaceString844 {
         char cht[] = t.toCharArray();
         Stack<Character> stks = new Stack<>();
         Stack<Character> stkt = new Stack<>();
-        int i=0;
-        while(i < lens) {
-            if (chs[i]!='#')
+        int i = 0;
+        while (i < lens) {
+            if (chs[i] != '#')
                 stks.push(chs[i]);
-            else if(!stks.empty() && chs[i]=='#')
+            else if (!stks.empty() && chs[i] == '#')
                 stks.pop();
             i++;
         }
-        i=0;
-        while(i < lent) {
-            if (cht[i]!='#')
+        i = 0;
+        while (i < lent) {
+            if (cht[i] != '#')
                 stkt.push(cht[i]);
-            else if(!stkt.empty() && cht[i]=='#')
+            else if (!stkt.empty() && cht[i] == '#')
                 stkt.pop();
             i++;
         }
-        if(stks.size()!=stkt.size())
+        if (stks.size() != stkt.size())
             return false;
-        if(stks.equals(stkt))
+        if (stks.equals(stkt))
             return true;
         return false;
     }
