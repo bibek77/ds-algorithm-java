0db57e99b92eabdbbe9aeb42f8be3122cad27f41
bibek77 <bibekrana01@gmail.com>
2022-09-16 12:04:20 +0530

    partition list

--- src/main/java/com/github/bibek77/dsa/miscellaneous/LinkedListImplement.java
+++ src/main/java/com/github/bibek77/dsa/miscellaneous/LinkedListImplement.java
@@ -6,19 +6,22 @@ package com.github.bibek77.dsa.miscellaneous;
 public class LinkedListImplement {
     public static void main(String[] args) {
         LinkedLists ll = new LinkedLists();
-        ll.createLL(3);
-        ll.insertNodeLL(4);
-        ll.insertNodeLL(1);
-        ll.insertNodeLL(3);
-        ll.insertNodeLL(6);
+        ll.createLL(1);
+        ll.insertNodeLL(9);
+        ll.insertNodeLL(5);
+        ll.insertNodeLL(10);
+        ll.insertNodeLL(2);
         ll.traverseLL();
         LinkedListsRemoveDuplicate linkedListsRemoveDuplicate = new LinkedListsRemoveDuplicate();
 //        linkedListsRemoveDuplicate.deleteDups(ll);
         LinkedListsNthToLast linkedListsNthToLast = new LinkedListsNthToLast();
-        int num = linkedListsNthToLast.nthToLast(ll, 5);
-        int numTwoPointer = linkedListsNthToLast.nthToLastTwoPointer(ll, 5);
-        System.out.println(num);
-        System.out.println(numTwoPointer);
+//        int num = linkedListsNthToLast.nthToLast(ll, 5);
+//        int numTwoPointer = linkedListsNthToLast.nthToLastTwoPointer(ll, 5);
+//        System.out.println(num);
+//        System.out.println(numTwoPointer);
+        ll.traverseLL();
+        LinkedListsPartition linkedListsPartition = new LinkedListsPartition();
+        linkedListsPartition.partition(ll, 4);
         ll.traverseLL();
     }
 
