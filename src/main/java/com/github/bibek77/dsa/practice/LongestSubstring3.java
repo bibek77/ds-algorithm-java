326461ea4ef14db8393d32d9b8210202cdcb2756
bibek77 <bibekrana01@gmail.com>
2022-09-18 20:05:29 +0530

    longest subsequence

--- src/main/java/com/github/bibek77/dsa/practice/LongestSubstring3.java
+++ src/main/java/com/github/bibek77/dsa/practice/LongestSubstring3.java
@@ -9,14 +9,35 @@ import java.util.Map;
 public class LongestSubstring3 {
 
     public static void main(String[] args) {
-        String word = "abcabcbb";
+        String word = "dvdfei";
         System.out.println(longestSubstringBrute(word));
+        System.out.println(longestSubstringOptimal(word));
+    }
+
+    public static int longestSubstringOptimal(String word) {
+        char ch[] = word.toCharArray();
+        int len = ch.length;
+        int pointer1 = 0, pointer2 = 0;
+        int maxcount = 0, count = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        if (len <= 1)
+            return len;
+        while (pointer2 < len) {
+            if (map.getOrDefault(ch[pointer2], -1) >= pointer1) {
+                pointer1 = map.get(ch[pointer2]) + 1;
+            }
+            map.put(ch[pointer2], pointer2);
+            maxcount = Math.max(maxcount, pointer2 - pointer1 + 1);
+            pointer2++;
+
+        }
+        return maxcount;
     }
 
     public static int longestSubstringBrute(String word) {
         char ch[] = word.toCharArray();
         int len = ch.length;
-        int maxcount = 0, count=0;
+        int maxcount = 0, count = 0;
         Map<Character, Boolean> map = new HashMap<>();
         for (int i = 0; i < len; i++) {
             map.clear();
@@ -32,6 +53,5 @@ public class LongestSubstring3 {
             }
         }
         return maxcount;
-
     }
 }
