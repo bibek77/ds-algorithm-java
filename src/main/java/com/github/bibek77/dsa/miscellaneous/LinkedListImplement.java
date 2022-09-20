4da428f70c95d16a83d73054f827f22d9255b613
bibek77 <bibekrana01@gmail.com>
2022-09-20 11:57:50 +0530

    reverse mn

--- src/main/java/com/github/bibek77/dsa/miscellaneous/LinkedListImplement.java
+++ src/main/java/com/github/bibek77/dsa/miscellaneous/LinkedListImplement.java
@@ -23,8 +23,11 @@ public class LinkedListImplement {
         LinkedListsPartition linkedListsPartition = new LinkedListsPartition();
         linkedListsPartition.partition(ll, 4);
         ll.traverseLL();
-        LinkedListReverse linkedListReverse = new LinkedListReverse();
-        linkedListReverse.reverseList(ll);
+//        LinkedListReverse linkedListReverse = new LinkedListReverse();
+//        linkedListReverse.reverseList(ll);
+//        ll.traverseLL();
+        LinkedListReverseMN linkedListReverseMN = new LinkedListReverseMN();
+        linkedListReverseMN.reverselistMN(ll, 2, 4);
         ll.traverseLL();
     }
 
