d03df369f83858ea33f75f513660a51bb40ce280
bibek77 <bibekrana01@gmail.com>
2022-09-30 23:49:40 +0530

    format text

--- src/main/java/com/github/bibek77/dsa/dataStructures/sorting/InsertionSort.java
+++ src/main/java/com/github/bibek77/dsa/dataStructures/sorting/InsertionSort.java
@@ -6,13 +6,12 @@ package com.github.bibek77.dsa.dataStructures.sorting;
 public class InsertionSort {
     public void insertionSort(int[] ar) {
         for (int i = 1; i < ar.length; i++) {
-            int temp = ar[i], j=i;
-            while(j>0 && ar[j-1]>temp) {
-                ar[j] = ar[j-1];
+            int temp = ar[i], j = i;
+            while (j > 0 && ar[j - 1] > temp) {
+                ar[j] = ar[j - 1];
                 j--;
             }
             ar[j] = temp;
         }
-
     }
 }
