6d0244d53bf6539fc1699cd5df11c112a7596b67
bibek77 <bibekrana01@gmail.com>
2022-10-03 20:24:59 +0530

    kthlargestinarray

---
+++ src/main/java/com/github/bibek77/dsa/practice/KthLargestinArray215.java
@@ -0,0 +1,45 @@
+package com.github.bibek77.dsa.practice;
+
+/**
+ * @author bibek
+ */
+public class KthLargestinArray215 {
+    public static void main(String[] args) {
+        int[] ar = {1, 4, 3, 40, 12, 8, 7, 34, 20};
+        System.out.println(findKthLargest(ar, 2));
+    }
+
+    public static int findKthLargest(int[] nums, int k) {
+
+        quickSort(nums, 0, nums.length - 1);
+        return nums[nums.length - k];
+
+    }
+
+    public static void quickSort(int[] arr, int start, int end) {
+        if (start < end) {
+            int pivot = partition(arr, start, end);
+            quickSort(arr, start, pivot - 1);
+            quickSort(arr, pivot + 1, end);
+        }
+    }
+
+    public static int partition(int[] arr, int start, int end) {
+        int pivot = end;
+        int i = start - 1;
+        for (int j = start; j < end; j++) {
+            if (arr[j] < arr[pivot]) {
+                i += 1;
+                swap(arr, i, j);
+            }
+        }
+        swap(arr, i + 1, pivot);
+        return i + 1;
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
