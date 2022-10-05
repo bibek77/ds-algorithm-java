4ddbae7ac67aa660b5ce07f191710ba2d0a4121f
bibek77 <bibekrana01@gmail.com>
2022-10-06 00:20:22 +0530

    binary tree implement

---
+++ src/main/java/com/github/bibek77/dsa/dataStructures/trees/binaryTrees/BinaryTreeLL.java
@@ -0,0 +1,11 @@
+package com.github.bibek77.dsa.dataStructures.trees.binaryTrees;
+
+/**
+ * @author bibek
+ */
+public class BinaryTreeLL {
+    BinaryNode root;
+    public BinaryTreeLL() {
+        this.root = null;
+    }
+}
---
+++ src/main/java/com/github/bibek77/dsa/dataStructures/trees/binaryTrees/BinaryTreeMain.java
@@ -0,0 +1,10 @@
+package com.github.bibek77.dsa.dataStructures.trees.binaryTrees;
+
+/**
+ * @author bibek
+ */
+public class BinaryTreeMain {
+    public static void main(String[] args) {
+        BinaryTreeLL binaryTreeLL = new BinaryTreeLL();
+    }
+}
