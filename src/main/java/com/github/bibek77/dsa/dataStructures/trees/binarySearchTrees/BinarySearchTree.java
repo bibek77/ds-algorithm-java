f6166d28662c36f75ca345d2c1ad2f2c514877fb
bibek77 <bibekrana01@gmail.com>
2022-10-08 12:29:01 +0530

    binary search tree insert

--- src/main/java/com/github/bibek77/dsa/dataStructures/trees/binarySearchTrees/BinarySearchTree.java
+++ src/main/java/com/github/bibek77/dsa/dataStructures/trees/binarySearchTrees/BinarySearchTree.java
@@ -9,4 +9,24 @@ public class BinarySearchTree {
     public BinarySearchTree() {
         root = null;
     }
+
+    //Insert Method
+    private BinaryNode insert(BinaryNode currentNode, int value) {
+        if (currentNode == null) {
+            BinaryNode newNode = new BinaryNode();
+            newNode.value = value;
+            System.out.println("Value successfully inserted at root");
+            return  newNode;
+        } else if (value <= currentNode.value) {
+            currentNode.left = insert(currentNode.left, value);
+            return currentNode;
+        } else {
+            currentNode.right = insert(currentNode.right, value);
+            return currentNode;
+        }
+    }
+
+    void insert(int value) {
+        insert(root, value);
+    }
 }
--- src/main/java/com/github/bibek77/dsa/dataStructures/trees/binarySearchTrees/BinarySearchTreeMain.java
+++ src/main/java/com/github/bibek77/dsa/dataStructures/trees/binarySearchTrees/BinarySearchTreeMain.java
@@ -6,5 +6,12 @@ package com.github.bibek77.dsa.dataStructures.trees.binarySearchTrees;
 public class BinarySearchTreeMain {
     public static void main(String[] args) {
         BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(70);
+        binarySearchTree.insert(30);
+        binarySearchTree.insert(50);
+        binarySearchTree.insert(20);
+        binarySearchTree.insert(60);
+        binarySearchTree.insert(90);
+        binarySearchTree.insert(100);
     }
 }
