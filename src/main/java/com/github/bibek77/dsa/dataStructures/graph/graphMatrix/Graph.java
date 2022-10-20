6f3f5b5c54d9b1e29e9fb9fe51ad604fd78b531d
bibek77 <bibekrana01@gmail.com>
2022-10-20 12:31:03 +0530

    bfs graph

--- src/main/java/com/github/bibek77/dsa/dataStructures/graph/graphMatrix/Graph.java
+++ src/main/java/com/github/bibek77/dsa/dataStructures/graph/graphMatrix/Graph.java
@@ -37,4 +37,44 @@ public class Graph {
         }
         return stringBuilder.toString();
     }
+
+    //Getting Neighbours
+    public ArrayList<GraphNode> getNeighbours(GraphNode node) {
+        ArrayList<GraphNode> neighbours = new ArrayList<>();
+        int nodeIndex = node.index;
+        // loop from first node to last node, ie equal to adjacency matrix col size
+        for (int i = 0; i < adjacencyMatrix.length; i++) {
+            if (adjacencyMatrix[nodeIndex][i] == 1) {
+                neighbours.add(nodeList.get(i));
+            }
+        }
+        return neighbours;
+    }
+
+    // helper method for BFS
+    public void bfsVisit(GraphNode node) {
+        Queue<GraphNode> queue = new LinkedList<>();
+        queue.add(node);
+        while (!queue.isEmpty()) {
+            GraphNode currentNode = queue.remove();
+            currentNode.isVisted = true;
+            System.out.print(currentNode.name + " ");
+            ArrayList<GraphNode> neighbours = getNeighbours(currentNode);
+            for (GraphNode neighbour : neighbours) {
+                if (!neighbour.isVisted) {
+                    queue.add(neighbour);
+                    neighbour.isVisted = true;
+                }
+            }
+        }
+    }
+
+    // BFS Main
+    public void bfs() {
+        for(GraphNode node : nodeList) {
+            if(!node.isVisted) {
+                bfsVisit(node);
+            }
+        }
+    }
 }
