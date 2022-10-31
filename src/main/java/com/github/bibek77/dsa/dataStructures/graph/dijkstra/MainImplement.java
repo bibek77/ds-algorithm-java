916c1ba22f5dad268063c82cb9f108bdb5198e61
bibek77 <bibekrana01@gmail.com>
2022-10-31 12:37:55 +0530

    dijkstra algo

---
+++ src/main/java/com/github/bibek77/dsa/dataStructures/graph/dijkstra/MainImplement.java
@@ -0,0 +1,10 @@
+package com.github.bibek77.dsa.dataStructures.graph.dijkstra;
+
+/**
+ * @author bibek
+ */
+public class MainImplement {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/github/bibek77/dsa/dataStructures/graph/dijkstra/WeightedGraph.java
@@ -0,0 +1,49 @@
+package com.github.bibek77.dsa.dataStructures.graph.dijkstra;
+
+import com.github.bibek77.dsa.dataStructures.graph.graphList.GraphNode;
+
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+/**
+ * @author bibek
+ */
+public class WeightedGraph {
+    ArrayList<WeightedNode> nodeList = new ArrayList<>();
+
+    public WeightedGraph(ArrayList<WeightedNode> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+    //Implement Dijkstra Algo
+    public void dijkstra(WeightedNode node) {
+        PriorityQueue<WeightedNode> queue = new PriorityQueue<>();
+        node.distance = 0;
+        queue.addAll(nodeList);
+        while (!queue.isEmpty()) {
+            WeightedNode currNode = queue.poll();
+            for (WeightedNode neighbor : currNode.neighbors) {
+                if (queue.contains(neighbor)) {
+                    if (neighbor.distance > currNode.distance + currNode.weightMap.get(neighbor)) {
+                        neighbor.distance = currNode.distance + currNode.weightMap.get(neighbor);
+                        neighbor.parent = currNode;
+                        queue.remove(neighbor);
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+        for (WeightedNode nodeCheck : nodeList) {
+            System.out.print("Node: " + nodeCheck + " , distance: " + nodeCheck.distance + ", Path: ");
+            pathPrint(node);
+            System.out.println();
+        }
+    }
+
+    public static void pathPrint(WeightedNode node) {
+        if (node.parent != null) {
+            pathPrint(node.parent);
+        }
+        System.out.print(node.name + " ");
+    }
+}
