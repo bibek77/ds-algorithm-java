c9f6dca73333c7e5d92175d4220335ec5bb6f987
bibek77 <bibekrana01@gmail.com>
2022-11-04 23:04:16 +0530

    topological sort bfs kahn

--- src/main/java/com/github/bibek77/dsa/miscellaneous/MainClass.java
+++ src/main/java/com/github/bibek77/dsa/miscellaneous/MainClass.java
@@ -1,10 +1,36 @@
 package com.github.bibek77.dsa.miscellaneous;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * @author bibek
  */
 public class MainClass {
     public static void main(String[] args) {
         MatrixDFS matrixDFS = new MatrixDFS();
+
+        System.out.println();
+        BfsKahnAlgorithm bfsKahnAlgorithm = new BfsKahnAlgorithm();
+        int nodes = 6;
+        ArrayList<Integer> adjNodes = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        adjList.add(new ArrayList<>());
+        adjList.add(new ArrayList<>());
+        adjNodes.add(3);
+        adjList.add(adjNodes);
+        adjNodes = new ArrayList<>();
+        adjNodes.add(1);
+        adjList.add(adjNodes);
+        adjNodes = new ArrayList<>();
+        adjNodes.add(0);
+        adjNodes.add(1);
+        adjList.add(adjNodes);
+        adjNodes = new ArrayList<>();
+        adjNodes.add(0);
+        adjNodes.add(2);
+        adjList.add(adjNodes);
+        int[] topologicalSort = bfsKahnAlgorithm.bfsKahnAlgorithm(nodes, adjList);
+        System.out.println(Arrays.toString(topologicalSort));
     }
 }
