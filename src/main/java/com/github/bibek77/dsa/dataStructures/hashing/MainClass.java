f2afd6078c7b734fcbd2ceb2c7fa9dad7c79ba25
bibek77 <bibekrana01@gmail.com>
2022-11-12 23:07:23 +0530

    hashtable

---
+++ src/main/java/com/github/bibek77/dsa/dataStructures/hashing/MainClass.java
@@ -0,0 +1,20 @@
+package com.github.bibek77.dsa.dataStructures.hashing;
+
+/**
+ * @author bibek
+ */
+public class MainClass {
+    public static void main(String[] args) {
+        DirectChaining directChaining = new DirectChaining(15);
+        directChaining.insertHashTable("The");
+        directChaining.insertHashTable("Quick");
+        directChaining.insertHashTable("Brown");
+        directChaining.insertHashTable("Fox");
+        directChaining.insertHashTable("Jumps");
+        directChaining.insertHashTable("Over");
+        directChaining.insertHashTable("The");
+        directChaining.insertHashTable("Lazy");
+        directChaining.insertHashTable("Dog");
+        directChaining.displayHashTable();
+    }
+}
