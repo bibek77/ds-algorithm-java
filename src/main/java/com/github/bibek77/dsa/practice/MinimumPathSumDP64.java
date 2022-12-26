2daff3796bd2c321e615d3bfea0ce12583780c2f
bibek77 <bibekrana01@gmail.com>
2022-12-26 16:10:12 +0530

    min path dp pattern

---
+++ src/main/java/com/github/bibek77/dsa/practice/MinimumPathSumDP64.java
@@ -0,0 +1,26 @@
+package com.github.bibek77.dsa.practice;
+
+/**
+ * @author bibek
+ */
+public class MinimumPathSumDP64 {
+    public int minPathSum(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 && j == 0) {
+                    // no change
+                } else if (i == 0 && j != 0) {
+                    grid[i][j] = grid[i][j] + grid[i][j - 1];
+                } else if (i != 0 && j == 0) {
+                    grid[i][j] = grid[i][j] + grid[i - 1][j];
+                } else {
+                    grid[i][j] = grid[i][j] + Math.min(grid[i - 1][j], grid[i][j - 1]);
+                }
+
+            }
+        }
+        return grid[n - 1][m - 1];
+    }
+}
