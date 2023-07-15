017b33eea6e888a32b292ca1359875e211f6f2bc
bibek77 <bibekrana01@gmail.com>
2023-07-15 20:27:23 +0530

    Num of Islands 200 using BFS

--- src/main/java/com/github/bibek77/dsa/practice/NumberOfIslands200.java
+++ src/main/java/com/github/bibek77/dsa/practice/NumberOfIslands200.java
@@ -50,4 +50,31 @@ public class NumberOfIslands200 {
         }
         System.out.println(islands);
     }
+
+    // Using BFS
+    public int numIslands(char[][] grid) {
+        int count = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(grid[i][j]=='1') {
+                    count+=1;
+                    callBfs(grid, i, j);
+
+                }
+            }
+        }
+        return count;
+    }
+
+    public void callBfs(char[][] grid, int i, int j) {
+        if(i<0 || i>=grid.length || j<0 || j>= grid[i].length || grid[i][j]=='0')
+            return;
+        grid[i][j]='0';
+        callBfs(grid, i+1, j);
+        callBfs(grid, i-1, j);
+        callBfs(grid, i, j-1);
+        callBfs(grid, i, j+1);
+    }
 }
