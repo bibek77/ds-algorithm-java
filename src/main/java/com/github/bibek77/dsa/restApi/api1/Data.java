42e4c78b4d7b3dccfeddd8ed37d930fcadc3e6e9
bibek77 <bibekrana01@gmail.com>
2023-10-30 11:19:55 +0530

    Hackerrank API1

---
+++ src/main/java/com/github/bibek77/dsa/restApi/api1/Data.java
@@ -0,0 +1,7 @@
+package com.github.bibek77.dsa.restApi.api1;
+
+public class Data {
+    Diagnosis diagnosis;
+    Doctor doctor;
+    Vitals vitals;
+}
---
+++ src/main/java/com/github/bibek77/dsa/restApi/api1/Vitals.java
@@ -0,0 +1,5 @@
+package com.github.bibek77.dsa.restApi.api1;
+
+public class Vitals {
+    int pulse;
+}
